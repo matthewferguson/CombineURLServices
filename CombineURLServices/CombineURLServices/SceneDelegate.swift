999333ec8532f06e8cae95ef31c1beef9306a9f0
Matthew Ferguson <matthew@mobilesandbox.com>
2025-03-02 17:55:47 -0500

    Adding SceneDelegate for multi device support and modernization

---
+++ CombineURLServices/CombineURLServices/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  CombineURLServices
+//
+//  Created by Matthew Ferguson on 3/2/25.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo
+               session: UISceneSession, options
+               connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = scene as? UIWindowScene else {return}
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+}
